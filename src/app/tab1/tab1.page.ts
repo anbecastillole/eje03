b59b62b4ba273e18534fd30443dca6fa3fa0038c
ADVABENAVIDESIB <advabenavidesib@ittepic.edu.mx>
2022-10-23 23:09:32 -0600

    mi aporte

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -20,6 +20,7 @@ export class Tab1Page {
     this.taskService.addTask(this.task);
     this.tasks=this.taskService.getTasks();
     console.log(this.tasks);
+    this.task='';
  
   }
 
@@ -30,7 +31,7 @@ export class Tab1Page {
 
   public completeTask(pos:number){
     //se agrega al otro
-    this.taskService.addCompletedTask(this.task);
+    this.taskService.addCompletedTask(pos);
     this.tasks = this.taskService.getTasks();
     //se elimina
     this.taskService.removeTask(pos);
